6733f8f92018a6ae551d7d32cbdfcdc86c6d571b
btang <btang@BERNARDTANG6841>
2019-09-04 12:43:34 +1000

    Initial commit.

--- CallbackSampleApp/Program.cs
+++ CallbackSampleApp/Program.cs
@@ -10,6 +10,34 @@ namespace CallbackSampleApp
     {
         static void Main(string[] args)
         {
+            Class2 c2 = new Class2();
+
+            /*
+            * Calling method on Class2 with callback method as parameter
+            */
+            c2.Method(CallBackMet);
+        }
+
+        /*
+        * The callback method. This method prints the string sent in the callback
+        */
+        static void CallBackMet(string str)
+        {
+            Console.WriteLine("Callback was: " + str);
+        }
+    }
+
+    public class Class2
+    {
+        /*
+        * The method that calls back to the caller. Takes an action (method) as parameter
+        */
+        public void Method(Action<string> callback)
+        {
+            /*
+            * Calls back to method CallBackMet in Class1 with the message specified
+            */
+            callback("The message to send back");
         }
     }
 }
